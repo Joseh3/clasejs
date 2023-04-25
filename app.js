61226a741b5eb608f3c731f52284c9bac1c25a7d
Jose Huamani <sv44834543@idat.edu.pe>
2023-04-24 21:53:08 -0500

    actualizando

---
+++ app.js
@@ -0,0 +1,40 @@
+//console.log('HOLA');
+
+//console.log('hola');
+//console.log('adios');
+
+// CREACIÓN DE VARIABLES
+
+//var una variable antigua no respetan el ambito del boque, const y let 
+
+// TIPO DE DATOS EN JS 
+
+//1. STRING 
+//2. NUMBER
+//3. BOOLEAN
+//4. NULL
+//5. UNDEFINED
+//6. OBJECT
+//7. ARRAY 
+
+let name = 'JOSE';
+let age=28;
+let logged=true;
+let notlogged=false;
+let lastName=null;
+let secondName;
+let string="cadena de texto";
+let Number=5;
+const PI=3.1415;
+
+
+//console.log(typeof name);
+//console.log(typeof age);
+//console.log(typeof logged);
+//console.log(typeof lastName);
+
+console.log(secondName);
+console.log(string);
+console.log(Number);
+console.log(PI);
+
